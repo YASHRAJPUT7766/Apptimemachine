15c3a43d86cae8df799036bfcef21a871d40a02a
Yash Rajput <yash92726@gmail.com>
2026-08-07 14:31:44 +0530

    Update AppNavigation.kt

--- app/src/main/java/com/apptimemachine/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/apptimemachine/ui/navigation/AppNavigation.kt
@@ -1,5 +1,6 @@
 package com.apptimemachine.ui.navigation
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
